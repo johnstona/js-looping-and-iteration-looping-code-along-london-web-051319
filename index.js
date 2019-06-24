19135763e3f0d3d3f7707e8c17209415109a9281
Alex Johnston <alexjohnston@ymail.com>
2019-06-24 11:32:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// const givers = ["Ada", "Brendan", "Ali"];
+let newArray = [];
+
+function writeCards(array, day) {
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${day} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(givers, day);
+
+function countdown(num) {
+    let countdown = num;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
